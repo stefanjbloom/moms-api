20527f31c706aeb5ef5ae551c93b0d5b414031a9
Stefan Bloom <stefanjbloom88@gmail.com>
2025-03-24 12:10:54 -0600

    init commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+//Main Starting point for app
+
+import express, { Application, Request, Response, urlencoded } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import routes from "./routes/index";
+
+// Loading env files from .env
+dotenv.config();
+
+// Initializing Express Application
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+// Security + Middleware
+app.use(cors()); //enable CORS
+app.use(helmet()); // add security headers
+app.use(morgan("dev")); // Log HTTP requests
+app.use(express.json()); //For parsing JSON request bodies
+app.use(urlencoded({ extended: true})) // Parse URL-encoded data
+
+// Rate Limiting to prevent abuse (100 req/15 mins)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, try again later."
+});
+app.use(limiter);
+
+// Register routes
+app.use("/api", routes)
+
+//Base route check
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "API is running" });
+});
+
+// Start express server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
---
+++ src/routes/index.ts
@@ -0,0 +1,1 @@
+// Centralized Router AFTER src/index
